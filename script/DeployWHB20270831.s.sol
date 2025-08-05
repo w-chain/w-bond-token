d6a34d427263220ca9502e757de712e87183ba8e
Evan Stinger <stefanustrg@gmail.com>
2025-08-05 13:24:20 +0800

    adjust WHB20270831 yield to 12.36%

--- script/DeployWHB20270831.s.sol
+++ script/DeployWHB20270831.s.sol
@@ -8,7 +8,7 @@ contract Deploy is Script {
     function run() public {
         address stakingACM = 0xfAc510D5dB8cadfF323D4b979D898dc38F3FB6dF;
         address treasuryAddress = 0xcBA04A89d8875f2eD85C91c8e856bE675e7BDA8c;
-        uint256 yield = 1234; // in BPS
+        uint256 yield = 1236; // 12.36% in BPS
         uint256 endOfBondingPeriod = 1756684799;
         uint256 maturityTimestamp = 1819756799;
 
